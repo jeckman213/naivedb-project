5d39d940f91ccf41b0274b25ccafcad97439eab8
Jonathan Eckman <je379138@csi402-9451-fa19.its.albany.edu>
2019-10-30 11:55:37 -0400

    Finished implementation of fileconverter, but still needs to be tied into main.c

--- fileconverter.c
+++ fileconverter.c
@@ -26,77 +26,85 @@ void fileconverter(char infile[20], char outdirectory[20])
 	const int SIZE_OF_RECORD = 29;
 	char *record = (char*) malloc(sizeof(char) * SIZE_OF_RECORD);
 	char *path = (char*) malloc(sizeof(char) * 30);
+	char *filePath = (char*) malloc(sizeof(char) * 30);
 
 	in = fopen(infile, "r");
 
 	// Creates file using create.c
 	create("-d", outdirectory);
 
+	// Creates basic path to directory (e.g. /OUT_DIRECTORY_NAME)
+	strncat(path, "./", 2);
+	strncat(path, outdirectory, strlen(outdirectory));
+
 	while (c != EOF)
 	{
-        c=getc(in);
-        if (c == '1')
-        {
-        	power = 7 - ((ftell(in) - 1) % 8); //ftell(first)==1->1000 0000->power=7-(1-1)=7-> 2^7
+        	c=getc(in);
+        	if (c == '1')
+        	{
+        		power = 7 - ((ftell(in) - 1) % 8); //ftell(first)==1->1000 0000->power=7-(1-1)=7-> 2^7
 			character += pow(2.0, (double) power); //adds all 1's to the character
-        }
-        counter++; //keeps track of bits
+        	}
+       		counter++; //keeps track of bits
 		if ((counter % 8) == 0) //after 8 bits -> convert the character and reset
 		{
-			printf("Got into append process\n");
 			char append = convert(character);
 			strncat(record, &append, 1);
 			character = 0;
 
 			if (strlen(record) < 3) 
 			{
-				printf("Got into determine airport\n");
-				strncat(path, "/", 1);
-				strncat(path, outdirectory, strlen(outdirectory));
-				
-				printf("Got past first strncat\n");	
+				printf("%s\n", path);
+
+				strcpy(filePath, path);	
 
 				if (strcmp(record, AMERICAN) == 0)
 				{
-					strncat(path, "/AA.txt", 8);
+					strncat(filePath, "/AA.txt", 8);
 				}
 				else if (strcmp(record, DELTA) == 0)
 				{
-					strncat(path, "/DL.txt", 8);	
+					strncat(filePath, "/DL.txt", 8);	
 				}
 				else if (strcmp(record, FRANCE) == 0)
 				{
-					strncat(path, "/F.txt", 7);
+					strncat(filePath, "/F.txt", 7);
 				}
 				else if (strcmp(record, SKYWEST) == 0)
 				{
-					strncat(path, "/OO.txt", 8);
+					strncat(filePath, "/OO.txt", 8);
 				} 
 				else 
 				{
-					strncat(path, "/z.txt", 7);
-					printf("Didn't find airline code\n");
+					continue;
 				}
 
-				printf("%s", path);
-				
-				creat(path, 0700);
+				printf("%s\n", filePath);
+
+				out = fopen(filePath, "a");
+
+				if (out == NULL)
+					printf("out = NULL\n");
+
+				memset(filePath, 0, strlen(filePath));
 			}
 
 			if (append == '\n') 
 			{
-				printf("Got to fprintf to txt file\n");
 				// Prints record out in text file
 				fprintf(out, record);
 
 				// New allocation of memory for record to reset
-				record = (char*) malloc(sizeof(char) * SIZE_OF_RECORD);
+				memset(record, 0, strlen(record));
 
 				// Closes currently opened file
 				fclose(out);
 			}
 		}
 	}
+	free(filePath);
+	free(record);
+	free(path);
 
 	// Closes opened files
 	fclose(in);
